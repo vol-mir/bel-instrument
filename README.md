56632288aa47d377e411830f5e6c55f9a13fd072
Vladzimir Bykovski <v.bykovski@e-vision.by>
2023-01-02 13:16:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Quick Start
+```shell
+$ git clone https://github.com/vol-mir/admin-bel-instrument.git
+$ cd admin-bel-instrument
+$ composer install
+$ cp .env.example .env
+$ php artisan key:generate
+$ php artisan migrate:refresh --seed
+```
---
+++ resources/views/dummy.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Большой выбор электроинструмента, бензоинструмента, ручного и садового инструмента, оснастки, товаров для дома и туризма, обогревателей, строительного оборудования, автотоваров и др. БелИнструментТорг, Майстар - Барановичи"/>
+    <meta name="keywords"
+          content="Барановичи, БелиИнструментТорг, Майстар, Строительный инструмент, Строительный инструмент купить, Строительные приборы, Инструмент, Электроинструмент, Бензопила штиль, Бензоинструмент, Газонокосилка, Шуруповерт, Перфоратор, Тепловые пушки, Обогреватели, Stihl, Makita, Bosch, Husqvarna, ECO, Partner, Viking"/>
+    <meta name="country" content="BY"/>
+    <meta name="author" content="БелиИнструментТорг"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- FAVICONS ICON -->
+    <link rel="icon" href="{{asset('dummy/images/favicon.ico')}}" type="image/x-icon"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('dummy/images/favicon.png')}}"/>
+
+    <!-- PAGE TITLE HERE -->
+    <title>БелиИнструментТорг</title>
+
+    <!-- MOBILE SPECIFIC -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="{{asset('dummy/js/html5shiv.min.js')}}"></script>
+    <script src="{{asset('dummy/js/respond.min.js')}}"></script>
+    <![endif]-->
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('dummy/css/plugins.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('dummy/css/style.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('dummy/css/templete.min.css')}}">
+    <link rel="stylesheet" type="text/css" class="skin" href="{{asset('dummy/css/skin/skin-9.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('dummy/css/jquery.localizationTool.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:400,400i,500,500i,700,700i&display=swap"
+          rel="stylesheet">
+
+</head>
+<body id="bg" class="font-poppins">
+<div id="loading-area">
+    <div></div>
+</div>
+<div class="page-wraper">
+    <canvas class="site-nav-canvas" style="display: none;"></canvas>
+    <!-- Content -->
+
+    <!-- Coming Soon Contant -->
+    <div class="dez-coming-soon style-9 wow fadeIn" data-wow-duration="0.80s" data-wow-delay="0.50s">
+        <video autoplay loop id="video-background" muted plays-inline>
+            <source src="{{asset('dummy/media/video3.mp4')}}" type="video/mp4">
+        </video>
+        <div class="clearfix dez-coming-bx">
+            <div class="dez-content top-left center-md">
+                <h2 class="dez-title ml2 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.5s"><span>Проект на стадии разработки </span><br/><span>БелИнструментТорг</span>
+                </h2>
+            </div>
+            <div class="text-right bottom-right">
+                <ul class="dez-social-icon social-2">
+                    <li class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1.0s"><a href="https://www.facebook.com/bel.instrument.by/"><i class="fa fa-facebook"></i><span>Facebook</span></a></li>
+                    <li class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1.6s"><a href="https://www.instagram.com/master.torgbar/"><i class="fa fa-instagram"></i><span>Instagram</span></a></li>
+                </ul>
+                <p class="copyright-text wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="2s"><span>© 2023 - БелиИнструментТорг</span><span
+                        class="heart"><i></i></span></p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- JavaScript  files ========================================= -->
+<script src="{{asset('dummy/js/jquery.min.js')}}"></script><!-- JQUERY.MIN JS -->
+<script src="{{asset('dummy/plugins/bootstrap/js/popper.min.js')}}"></script><!-- POPPER.MIN JS -->
+<script src="{{asset('dummy/plugins/bootstrap/js/bootstrap.min.js')}}"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="{{asset('dummy/plugins/countdown/jquery.countdown.js')}}"></script><!-- JQUERY COUNTDOWN -->
+<script src="{{asset('dummy/plugins/scroll/scrollbar.min.js')}}"></script><!-- SCROLLBAR -->
+<script src="{{asset('dummy/plugins/imagesloaded/imagesloaded.js')}}"></script><!-- IMAGESLOADED -->
+<script src="{{asset('dummy/plugins/masonry/masonry-3.1.4.js')}}"></script><!-- MASONRY -->
+<script src="{{asset('dummy/plugins/masonry/masonry.filter.js')}}"></script><!-- MASONRY -->
+<script src="{{asset('dummy/plugins/lightgallery/js/lightgallery-all.min.js')}}"></script><!-- LIGHTGALLERY -->
+<script src="{{asset('dummy/js/jquery.localizationTool.js')}}"></script><!-- localizationTool JS -->
+<script src="{{asset('dummy/js/translator.js')}}"></script><!-- Translator JS -->
+<script src="{{asset('dummy/js/custom.min.js')}}"></script><!-- CUSTOM FUCTIONS  -->
+<script src="{{asset('dummy/js/dz.ajax.js')}}"></script><!-- CONTACT JS  -->
+<script src="{{asset('dummy/js/wow.js')}}"></script><!-- WOW JS -->
+<script src="{{asset('dummy/plugins/anime/anime.min.js')}}"></script><!-- CUSTOM FUCTIONS  -->
+<script src="{{asset('dummy/plugins/anime/app.js')}}"></script><!-- CUSTOM FUCTIONS  -->
+<script src='https://www.google.com/recaptcha/api.js'></script> <!-- Google API For Recaptcha  -->
+<div class="site-nav-overlay js-nav" style="opacity: 0;"></div>
+</body>
+</html>
